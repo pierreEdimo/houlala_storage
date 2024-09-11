4ea381a4fb3444b62b3eabf313bca5a3a2c1aa00
pnk <pierre.nkoe@vema-eg.de>
2024-09-12 00:21:52 +0200

    system-datei

--- obj/Release/net8.0/houlala_storage.AssemblyInfo.cs
+++ obj/Release/net8.0/houlala_storage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("houlala_storage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af64f482b2e88bb63df0efa0a73871517707df4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc5ebba2a5e699aeb4219a548a38dc064d8b2d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("houlala_storage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("houlala_storage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
